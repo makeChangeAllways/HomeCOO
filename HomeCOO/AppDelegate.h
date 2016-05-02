2b59973cfd540afae54a4c459a2d78f211078813
tgbus <tgbus@tgbusdemacbook-pro.local>
2016-05-02 19:36:32 +0800

    Initial Commit

---
+++ HomeCOO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeCOO
+//
+//  Created by tgbus on 16/5/2.
+//  Copyright © 2016年 Jiaoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
